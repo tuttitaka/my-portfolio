06c9bad8810befc87cb0508ec2231a280dfa2922
Luis Eduardo <luiseduardf.a@gmail.com>
2024-06-12 16:46:10 -0300

    a

--- my-portfolio/src/Carousel/Carousel.tsx
+++ my-portfolio/src/Carousel/Carousel.tsx
@@ -1,49 +1,51 @@
+import React, { useEffect, useRef, useState } from "react";
+import * as S from "./styles";
+import { motion } from "framer-motion";
+import img1 from "../Assets/img/cordel.png";
+import img2 from "../Assets/img/mascote.png";
+import img3 from "../Assets/img/social.png";
 
+const imgs = [
+  { src: img1, url: 'https://tuttitaka.github.io/projeto-android/', description: 'Projeto Cordel' },
+  { src: img2, url: 'https://example.com/link2', description: 'Projeto Mascote' },
+  { src: img3, url: 'https://example.com/link3', description: 'Projeto Social' }
+];
 
-import React, { useEffect, useRef, useState } from "react"
-import * as S from "./styles"
-import {motion} from "framer-motion"
-import img1 from "../Assets/img/cordel.png"
-import img2 from "../Assets/img/mascote.png"
-import img3 from "../Assets/img/social.png"
+export const Carousel: React.FC = () => {
+  const carousel = useRef<HTMLDivElement>(null);
+  const [width, setWidth] = useState(0);
 
-const imgs = [img1, img2, img3]
+  useEffect(() => {
+    if (carousel.current) {
+      console.log(carousel.current.scrollWidth, carousel.current.offsetWidth);
+      setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+    }
+  }, []);
 
-
-export const Carousel: React.FC = () =>{
-    const carousel = useRef<HTMLDivElement>(null);
-    const [width, setWidth] = useState(0)
-
-
-    useEffect(() => {
-        if (carousel.current) {
-            console.log(carousel.current.scrollWidth, carousel.current.offsetWidth);
-            setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
-        }
-    }, []);
-
-
-    return (
-        <motion.div whileTap={{cursor: "grabbing"}}>
-            <S.WrapperCarousel >
-                <S.WrapperItens>
-                    <motion.div drag ="x" dragConstraints ={{ right: 0, left: -width}}
-                    initial= {{x:300}}
-                    animate= {{x:0}}
-                    transition={{duration : 0.9}}>
-                        <S.Itens>
-                            {imgs.map(image => (
-                            <motion.div key={image}>
-                                <img src= {image} alt="" />
-                            </motion.div>
-                        ))}
-                        </S.Itens>
-                    </motion.div>
-                </S.WrapperItens>
-            </S.WrapperCarousel>
+  return (
+    <motion.div whileTap={{ cursor: "grabbing" }}>
+      <S.WrapperCarousel ref={carousel}>
+        <motion.div
+          drag="x" 
+          dragConstraints={{ right: 0, left: -width }}
+          initial={{ x: 300 }}
+          animate={{ x: 0 }}
+          transition={{ duration: 0.9 }}
+        >
+          <S.Itens>
+            {imgs.map((image, index) => (
+              <S.Item key={index}>
+                <img src= {image.src} alt="" />
+                <a href={image.url} target="_blank" rel="noopener noreferrer">
+                  <S.Description>{image.description}</S.Description>
+                </a>
+              </S.Item>
+            ))}
+          </S.Itens>
         </motion.div>
+      </S.WrapperCarousel>
+    </motion.div>
+  );
+};
 
-    )
-
-
-}
+export default Carousel;
--- my-portfolio/src/Carousel/styles.ts
+++ my-portfolio/src/Carousel/styles.ts
@@ -1,35 +1,54 @@
 import styled from "styled-components";
-
-
-
+ 
 export const WrapperCarousel = styled.div`
-    margin: 0 auto;
-    display: flex;
-    align-items: center;
-    width: 200%;
-    max-width: 1000px;
-    cursor: grab;
-    overflow: hidden;
-    justify-content: center;
-    
-`
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  width: 200%;
+  max-width: 1000px;
+  overflow: hidden;
+  justify-content: center;
+  cursor: grab;
+`;
+
 export const WrapperItens = styled.div`
+  display: flex;
+`;
 
-
-
-`
 export const Itens = styled.div`
-    
-    display: flex;
-    min-height: 200px;
-    min-width: 400px;
-    gap: 3rem;
+  display: flex;
+  min-height: 200px;
+  min-width: 400px;
+  gap: 3rem;
 
-    img{
-        width: 100%;
-        height: 90%;
-        border-radius: 10px;
-        pointer-events: none;
-    }
+  img {
+    display: block;
+    width: 100%;
+    height: 90%;
+    border-radius: 10px;
+    pointer-events: none;
+  }
+`;
 
-`
+export const Item = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0 10px;
+`;
+
+export const Description = styled.div`
+  margin-top: 10px;
+  font-size: 16px;
+  color: white;
+  text-align: center;
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  a:hover {
+    text-decoration: underline;
+  }
+`;
